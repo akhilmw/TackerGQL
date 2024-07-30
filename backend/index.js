8247ba703583c8996002d9af46a1e04ddfdf0dca
Akhil.Nair <akhilmw2@gmail.com>
2024-07-30 16:45:18 +0530

    Db config issue resolved

--- backend/index.js
+++ backend/index.js
@@ -16,7 +16,7 @@ import { buildContext } from "graphql-passport";
 import mergedResolvers from "./resolvers/index.js";
 import mergedTypeDefs from "./typeDefs/index.js";
 
-import { connectDB } from "./db/connectDB.js";
+import { connectDB } from "./db/connectDb.js";
 import { configurePassport } from "./passport/passport.config.js";
 
 
